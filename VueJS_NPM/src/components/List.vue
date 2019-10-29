0a6b7b35941a6f7e86db479f7feff894af4826d9
tterpi <the.o@live.de>
2019-10-29 23:21:36 +0100

    Created a Vue CLI project for the todo app

---
+++ VueJS_NPM/src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<ul>
+			<ListItem
+				v-for="(item, index) in todos"
+				v-bind:todo="item"
+				v-bind:key="item.id"
+				v-on:delete-todo="$emit('delete-todo',index)"
+				v-on:save-todo="$emit('save-todo',{index: index, message: $event})"
+			/>
+		</ul>
+		<button v-on:click="$emit('add-todo')">Add todo</button>
+		</div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+
+export default{
+	components: {ListItem},
+	props: ['todos']
+}
+</script>
+
