e8b6b5ad64baaf485c5db5c2cafbf78665e471ce
tterpi <the.o@live.de>
2019-11-09 17:12:24 +0100

    Test for displaying of todo text.

--- 2/src/components/__tests__/ListItem.spec.js
+++ 2/src/components/__tests__/ListItem.spec.js
@@ -1,17 +1,29 @@
 ﻿import { shallowMount } from '@vue/test-utils'
 import ListItem from '../ListItem.vue'
 
+const factory = (values = {}) => {
+  return shallowMount(ListItem, {
+    propsData : values
+  })
+}
+
 describe('ListItem', () => {
 	it('Renders a delete button', () => {
-		const wrapper = shallowMount(ListItem, {
-			propsData: {
+		const wrapper = factory({
 				todo: { id: '1', message: 'Hello', },
-			}
 		})
 
 		const deleteButton = wrapper.find('#deleteButton')
 
 		expect(deleteButton.exists()).toBeTruthy()
 		expect(deleteButton.text()).toContain('Delete');
+	})
+
+	it('Renders the todo text', () => {
+		const wrapper = factory({
+				todo: { id: '1', message: 'Hello', },
+		})
+
+		expect(wrapper.text()).toContain('Hello');
 	})
 })
