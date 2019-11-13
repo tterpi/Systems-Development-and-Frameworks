5ff4ac7e6d915dd73b1c42196896e1a6c055fd2f
tterpi <the.o@live.de>
2019-11-13 17:25:08 +0100

    Moved functionality from app.vue to List.vue.

--- 2/src/app.vue
+++ 2/src/app.vue
@@ -1,44 +1,10 @@
 <template>
-	<List
-		v-bind:todos="todos"
-		v-on:add-todo="addTodo"
-		v-on:save-todo="saveTodo"
-		v-on:delete-todo="deleteTodo"
-	></List>
+	<List></List>
 </template>
 
 <script>
 import List from './components/List.vue'
 export default{
   components: {List},
-  methods: {
-	addTodo: function(){
-		this.todos.push(
-			{ id: this.getNextId(), message: '', }
-		);
-	},
-	saveTodo: function(todo){
-		this.todos[todo.index].message = todo.message;
-	},
-	deleteTodo: function(index){
-		this.todos.splice(index, 1);
-	},
-	getNextId: function(){
-		return this.nextId++;
-	}
-  },
-  created: function(){
-	if(this.nextId === null){
-			this.nextId = this.todos.length+1;
-	}
-  },
-  data: function(){return{
-    todos: [
-      { id: '1', message: 'Foo', },
-      { id: '2', message: 'Bar', },
-      { id: '3', message: 'Baz', }
-    ],
-	nextId: null,
-  }},
 }
 </script>
--- 2/src/components/List.vue
+++ 2/src/components/List.vue
@@ -5,20 +5,50 @@
 				v-for="(item, index) in todos"
 				v-bind:todo="item"
 				v-bind:key="item.id"
-				v-on:delete-todo="$emit('delete-todo',index)"
-				v-on:save-todo="$emit('save-todo',{index: index, message: $event})"
+				v-on:delete-todo="deleteTodo(index)"
+				v-on:save-todo="saveTodo({index: index, message: $event})"
 			/>
 		</ul>
-		<button class="add-button" v-on:click="$emit('add-todo')">Add todo</button>
+		<button class="add-button" v-on:click="addTodo()">Add todo</button>
 	</div>
 </template>
 
 <script>
 import ListItem from './ListItem.vue'
 
-export default{
-	components: {ListItem},
-	props: ['todos']
+export default {
+    components: { ListItem },
+    methods: {
+        addTodo: function () {
+            this.todos.push(
+                { id: this.getNextId(), message: '', }
+            );
+        },
+        saveTodo: function (todo) {
+            this.todos[todo.index].message = todo.message;
+        },
+        deleteTodo: function (index) {
+            this.todos.splice(index, 1);
+        },
+        getNextId: function () {
+            return this.nextId++;
+        }
+    },
+    created: function () {
+        if (this.nextId === null) {
+            this.nextId = this.todos.length + 1;
+        }
+    },
+    data: function () {
+        return {
+            todos: [
+                { id: '1', message: 'Foo', },
+                { id: '2', message: 'Bar', },
+                { id: '3', message: 'Baz', }
+            ],
+            nextId: null,
+        }
+    },
 }
 </script>
 
